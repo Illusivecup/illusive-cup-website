9979e852fb6f5688e1e745464499645581d27423
Illusivecup <Illusivecup@users.noreply.github.com>
2025-11-29 00:56:20 +0000

    deploy: a7d78aafa8825ed9bc32064a615bcc813cb5432f

---
+++ admin-config.js
@@ -0,0 +1,16 @@
+// admin-config.example.js
+// 🛑 НЕ КОММИТЬТЕ СВОЙ РЕАЛЬНЫЙ ПАРОЛЬ!
+// Скопируйте этот файл в admin-config.js и добавьте свои данные.
+
+window.ADMIN_CONFIG = {
+    editorPassword: "IllusiveCup2025!",
+    firebaseConfig: {
+        apiKey: "AIzaSyAjUOjB-mQTdI6G4jwsIXGOHGldGBmC6j4",
+        authDomain: "illusive-cup.firebaseapp.com",
+        databaseURL: "https://illusive-cup-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "illusive-cup",
+        storageBucket: "illusive-cup.firebasestorage.app",
+        messagingSenderId: "465786550229",   
+        appId: "1:465786550229:web:9a1d4a3015b9cb0a3caf5c"
+    }
+};
